919d7670354e8c7323e5efa4e109b2bb4878dbf2
SarahZ <szachrich@yahoo.com>
2020-09-15 18:38:50 -0600

    no format html & app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_costa
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def index():
+    mars = mongo.db.collection.find_one()
+    return render_template("index.html", mars=mars)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.replace_one({}, mars_data, upsert=True)
+    return "Scraping Successful!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
